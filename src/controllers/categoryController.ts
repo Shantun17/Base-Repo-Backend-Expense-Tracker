a0ae17e470517bca3afab9b8ab029a3fa36c3425
Shantun Kumar <shantun.kumar@cimpress.com>
2025-05-23 15:20:55 +0530

    Added a validation to check whether category already exists or not

--- src/controllers/categoryController.ts
+++ src/controllers/categoryController.ts
@@ -4,37 +4,42 @@ import { insertCategory, getUserCategory } from '../dbHelper/categoryDBHelper.ts
 
 
 export const addCategory = async (
-    req : AuthenticatedRequest,
-    res : Response,
-    next : NextFunction
+  req : AuthenticatedRequest,
+  res : Response,
+  next : NextFunction
 ): Promise<void> => {
-    const {name, type} = req.body;
-    const userId = req.user?.userId;
+  const {name, type} = req.body;
+  const userId = req.user?.userId;
 
 
-  if (!name) {
-    res.status(400).json({ error: 'Category name is required' });
+if (!name) {
+  res.status(400).json({ error: 'Category name is required' });
+  return;
+}
+
+if (!type) {
+  res.status(400).json({ error: 'Category Type is required' });
+  return;
+}
+try {
+  const exists = await categoryExists(userId!, name, type);
+  if (exists) {
+    res.status(400).json({ error: 'Category already exists' });
+    return;
+  }
+  await insertCategory(userId!,name,type);
+  res.status(201).json({ message: 'Category added successfully' });
+} catch (error: any) {
+  console.error('Error adding category:', error);
+
+  if (error.code === 'ECONNREFUSED') {
+    res.status(503).json({ error: 'Service is temporarily unavailable. Please try again later.' });
     return;
   }
 
-  if (!type) {
-    res.status(400).json({ error: 'Category Type is required' });
-    return;
-  }
-  try {
-    await insertCategory(userId!, name, type)
-    res.status(201).json({ message: 'Category added successfully' });
-  } catch (error: any) {
-    console.error('Error adding category:', error);
-
-    if (error.code === 'ECONNREFUSED') {
-      res.status(503).json({ error: 'Service is temporarily unavailable. Please try again later.' });
-      return;
-    }
-
-    res.status(500).json({ error: 'An unexpected error occurred. Please try again later.' });
-    next(error);
-  }
+  res.status(500).json({ error: 'An unexpected error occurred. Please try again later.' });
+  next(error);
+}
 };
 
 export const getCategories = async (
