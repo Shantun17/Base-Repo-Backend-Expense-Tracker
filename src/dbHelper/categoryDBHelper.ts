6a0de92890585c337090df35e21d3f69f76d08cb
Shantun Kumar <shantun.kumar@cimpress.com>
2025-06-04 18:39:15 +0530

    correct the DBHelpers insert category function

--- src/dbHelper/categoryDBHelper.ts
+++ src/dbHelper/categoryDBHelper.ts
@@ -1,8 +1,8 @@
 import { pool } from '../models/db.ts';
 import { insertCategoryQuery,getUserCategoriesQuery,checkCategoryExistsQuery} from '../queries/categoryQueries.ts';
 
-export const insertCategory = async (userId: number, name: string, type: string) => {
-  return await pool.query(insertCategoryQuery, [userId, name, type]);
+export const insertCategory = (userId: number, name: string, type: string) => {
+  return pool.query(insertCategoryQuery, [userId, name, type]);
 };
 
 export const getUserCategories = (userId: number)=>{
